bcb7ed8bfddd332c342557f1d21a1d2897da2f5b
tomashina <tomislav@agmedia.hr>
2025-08-07 19:07:20 +0200

    slider

--- resources/views/front/catalog/product/index.blade.php
+++ resources/views/front/catalog/product/index.blade.php
@@ -301,7 +301,7 @@
 <h2 class="h3 mb-4 pt-1 font-title me-3 text-center"> Povezani artikli</h2>
 
 </div>
-<div class="tns-carousel tns-controls-static tns-controls-outside tns-nav-enabled pt-2">
+    <div class="tns-carousel pt-2 pb-2">
 <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 2, &quot;gutter&quot;: 16, &quot;controls&quot;: true, &quot;autoHeight&quot;: true, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:2}, &quot;480&quot;:{&quot;items&quot;:2}, &quot;720&quot;:{&quot;items&quot;:3}, &quot;991&quot;:{&quot;items&quot;:2}, &quot;1140&quot;:{&quot;items&quot;:3}, &quot;1300&quot;:{&quot;items&quot;:4}, &quot;1500&quot;:{&quot;items&quot;:5}}}">
     @foreach ($cat->products()->get()->unique()->take(10) as $cat_product)
         @if ($cat_product->id  != $prod->id)
--- resources/views/front/layouts/widget/widget_product_carousel.blade.php
+++ resources/views/front/layouts/widget/widget_product_carousel.blade.php
@@ -12,7 +12,7 @@
 
     </div>
     <div class="tns-carousel pt-2 pb-2">
-        <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 2, &quot;gutter&quot;: 15, &quot;controls&quot;: true, &quot;nav&quot;: true, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1},&quot;500&quot;:{&quot;items&quot;:2},&quot;768&quot;:{&quot;items&quot;:2}, &quot;992&quot;:{&quot;items&quot;:5, &quot;gutter&quot;: 30}}}">
+        <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 2, &quot;gutter&quot;: 16, &quot;controls&quot;: true, &quot;autoHeight&quot;: true, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:2}, &quot;480&quot;:{&quot;items&quot;:2}, &quot;720&quot;:{&quot;items&quot;:3}, &quot;991&quot;:{&quot;items&quot;:2}, &quot;1140&quot;:{&quot;items&quot;:3}, &quot;1300&quot;:{&quot;items&quot;:4}, &quot;1500&quot;:{&quot;items&quot;:5}}}">
             @foreach ($data['items'] as $product)
                 <!-- Product-->
                 <div>
