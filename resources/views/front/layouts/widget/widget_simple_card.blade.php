a8e5cae5cd76baa728bab90db590c74181ee878a
tomashina <tomislav@agmedia.hr>
2025-08-08 10:14:08 +0200

    viewed fix

--- resources/views/front/layouts/widget/widget_simple_card.blade.php
+++ resources/views/front/layouts/widget/widget_simple_card.blade.php
@@ -1,11 +1,11 @@
 <!-- {"title": "Banneri", "description": "Widget za bannere"} -->
-<section class=" py-3 fbck " >
+<section class=" py-3  " >
 
 
     <div class="row  mt-2 mt-lg-3 ">
         @foreach ($data as $widget)
             <div class="col-lg-12 col-xl-{{ $widget['width'] }} mb-grid-gutter">
-                <div class="d-block d-sm-flex justify-content-between align-items-center   @if ($widget == end($data)) bg-faded-info @else bg-faded-warning @endif  rounded-3">
+                <div class="d-block d-sm-flex justify-content-between align-items-center   fbck  rounded-3">
                         <div class="pt-5 py-sm-4 px-4 ps-md-4 pe-md-0 text-center text-sm-start">
                                 <h2 class="font-title">{{ $widget['title'] }}</h2>
                                 <p class="text-muted pb-2">{{ $widget['subtitle'] }}</p><a class="btn btn-primary" href="{{ url($widget['url']) }}">Pogledajte ponudu <i class="ci-arrow-right ms-2 me-n1"></i></a>
