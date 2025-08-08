34803a2df6a006dee97d698cbf6372f72774f60d
tomashina <tomislav@agmedia.hr>
2025-08-08 07:18:03 +0200

    fix slider products

--- resources/views/front/catalog/product/index.blade.php
+++ resources/views/front/catalog/product/index.blade.php
@@ -302,7 +302,7 @@
 
 </div>
     <div class="tns-carousel pt-2 pb-2">
-<div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 2, &quot;gutter&quot;: 16, &quot;controls&quot;: true,  &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:2}, &quot;480&quot;:{&quot;items&quot;:2}, &quot;720&quot;:{&quot;items&quot;:3}, &quot;991&quot;:{&quot;items&quot;:2}, &quot;1140&quot;:{&quot;items&quot;:3}, &quot;1300&quot;:{&quot;items&quot;:4}, &quot;1500&quot;:{&quot;items&quot;:5}}}">
+        <div class="tns-carousel-inner" data-carousel-options='{"items": 2, "controls": true, "nav": true, "responsive": {"0":{"items":2, "gutter": 10},"500":{"items":2, "gutter": 10},"768":{"items":3, "gutter": 10}, "1100":{"items":4, "gutter": 10}, "1500":{"items":5, "gutter": 10}, "1600":{"items":6, "gutter": 10}}}'>
     @foreach ($cat->products()->get()->unique()->take(10) as $cat_product)
         @if ($cat_product->id  != $prod->id)
             <div>
