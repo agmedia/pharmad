45b42855457be84d6a63c9c82b2f24adce29dccb
tomashina <tomislav@agmedia.hr>
2025-08-08 10:21:07 +0200

    viewed fix

--- resources/views/front/layouts/widget/widget_simple_card.blade.php
+++ resources/views/front/layouts/widget/widget_simple_card.blade.php
@@ -7,8 +7,9 @@
             <div class="col-lg-12 col-xl-{{ $widget['width'] }} mb-grid-gutter">
                 <div class="d-block d-sm-flex justify-content-between align-items-center   fbck  rounded-3">
                         <div class="pt-5 py-sm-4 px-4 ps-md-4 pe-md-0 text-center text-sm-start">
-                                <h2 class="font-title">{{ $widget['title'] }}</h2>
-                                <p class="text-muted pb-2">{{ $widget['subtitle'] }}</p><a class="btn btn-primary" href="{{ url($widget['url']) }}">Pogledajte ponudu <i class="ci-arrow-right ms-2 me-n1"></i></a>
+                                <h3 class="font-title">{{ $widget['title'] }}</h3>
+                                <p class="text-muted fs-md pb-2">{{ $widget['subtitle'] }}</p>
+                            <a class="btn btn-outline-primary btn-sm btn-shadow mt-3" href="{{ url($widget['url']) }}">Pogledajte ponudu <i class="ci-arrow-right ms-2 me-n1"></i></a>
                         </div>
                        <img class="d-block mx-auto mx-sm-0 rounded-end" src="{{ $widget['image'] }}" width="220" height="263" alt="{{ $widget['title'] }}">
                 </div>
