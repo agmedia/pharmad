f78888267c5efa069f5b4d91bee21b20eaff7324
tomashina <tomislav@agmedia.hr>
2025-08-12 10:12:16 +0200

    fix brandovi typo

--- resources/views/front/layouts/widget/widget_page_carousel.blade.php
+++ resources/views/front/layouts/widget/widget_page_carousel.blade.php
@@ -33,7 +33,7 @@
 
 
         <div class="tns-carousel">
-            <div class="tns-carousel-inner" data-carousel-options='{"items": 2, "controls": true, "autoHeight": false, "responsive": {"0":{"items":2, "gutter": 10},"480":{"items":2, "gutter": 10},"800":{"items":3, "gutter": 20}, "1200":{"items":4, "gutter": 30}, "1800":{"items":5, "gutter": 30}}}'>
+            <div class="tns-carousel-inner" data-carousel-options='{"items": 2, "controls": true, "autoHeight": false, "responsive": {"0":{"items":2, "gutter": 10},"480":{"items":2, "gutter": 10},"800":{"items":3, "gutter": 20}, "1200":{"items":4, "gutter": 30}, "1500":{"items":5, "gutter": 30}}}'>
                 @foreach ($data['items'] as $item)
                     <div class="col-md-3 col-sm-4 col-6"><a class="d-block bg-white shadow-sm rounded-3 py-3 py-sm-4 mb-grid-gutter" href="{{ $item['url'] }}"><img loading="lazy" class="d-block mx-auto" src="{{ $item['image'] }}" style="width: 150px;" alt="{{ $item['title'] }}"></a></div>
                 @endforeach
