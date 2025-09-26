ac1b2cf7b9328a3433bbdaf1902906d92cc0edb0
tomashina <tomislav@agmedia.hr>
2025-09-26 20:21:17 +0200

    gogole

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -81,22 +81,15 @@
     @if (config('app.env') == 'production')
         @yield('google_data_layer')
         <!-- Google Tag Manager -->
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-xxxxxxxx');</script>
-        <!-- End Google Tag Manager -->
 
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <!--   <script async src="https://www.googletagmanager.com/gtag/js?id=xxxxxxx"></script>-->
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-L62GT8MBLB"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', ' G-xxxxxxxx');
+            gtag('config', 'G-L62GT8MBLB');
         </script>
 
 
@@ -107,12 +100,7 @@
 
 <!-- Body-->
 <body class="bg-bck">
-@if (config('app.env') == 'production')
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-xxxxxxx"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-@endif
+
 <div id="agapp">
     <div v-cloak>
 
