0ad8588d26bcb09ff746228fcff835dd0f7eae13
tomashina <tomislav@agmedia.hr>
2025-10-08 15:53:08 +0200

    gogole

--- resources/views/front/catalog/category/index.blade.php
+++ resources/views/front/catalog/category/index.blade.php
@@ -157,7 +157,7 @@
         </div>
     @endif
 
-    <div class="container pb-4 mb-2 mt-5 mb-md-4" >
+    <div class="container-fluid pb-4 mb-2 mt-5 mb-md-4" >
         @if ($cat && !$subcat)
             {!! $cat->description !!}
         @elseif ($subcat)
