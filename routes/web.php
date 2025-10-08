21f6ae5c36cf2d59dc0c41ffac3aa0eb0822007c
tomashina <tomislav@agmedia.hr>
2025-10-08 15:33:17 +0200

    gogole

--- routes/web.php
+++ routes/web.php
@@ -361,7 +361,7 @@ Route::get('cache/thumb', [HomeController::class, 'thumbCache']);
  */
 Route::redirect('/sitemap.xml', '/sitemap');
 Route::get('sitemap/{sitemap?}', [HomeController::class, 'sitemapXML'])->name('sitemap');
-Route::get('image-sitemap', [HomeController::class, 'sitemapImageXML'])->name('sitemap');
+Route::get('image-sitemap', [HomeController::class, 'sitemapImageXML'])->name('sitemap.image');
 //
 Route::get('njuskalo/xml', [HomeController::class, 'njuskaloXML'])->name('njuskalo');
 
