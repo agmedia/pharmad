bc20dd84d0506f8bc643ed94fe0c335eb2f375a0
tomashina <tomislav@agmedia.hr>
2025-12-05 09:40:25 +0100

    brand

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -391,7 +391,7 @@ class DashboardController extends Controller
             $stock    = (int)   ($item->Stock ?? 0);
 
             // SKIP: Yasenka u nazivu (toleriramo i tipkarsku "yasenbka")
-            $title = trim((string) ($item->Title ?? ''));
+          /*  $title = trim((string) ($item->Title ?? ''));
             if ($title !== '' && (
                     stripos($title, 'yasenka') !== false ||
                     stripos($title, 'yasenbka') !== false
@@ -399,7 +399,7 @@ class DashboardController extends Controller
             ) {
                 $skippedCount++;
                 continue;
-            }
+            }*/
 
             // samo stvarne akcije
             if ($regular <= 0 || $sale <= 0 || $sale >= $regular) {
